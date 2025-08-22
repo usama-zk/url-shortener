b630434bc3afe2cf23fdc158bbc577e094476e2d
Usama Mehmood <154636910+usama-zk@users.noreply.github.com>
2025-08-22 17:24:19 +0500

    changes

--- connect.js
+++ connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connectDB() {
   try {
     await mongoose.connect(
-      "mongodb+srv://usamamehmood:pass123@cluster0.pdxuals.mongodb.net/urlShortener",
+      "<mongo DB URL>",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
